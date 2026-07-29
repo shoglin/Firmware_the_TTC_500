3fb9d9df9a2ea89b24bafa9669f5c315ccdc1962
Максим Шоглин <shoglin@KBSKAZKA.local>
2026-07-29 11:01:29 +0300

    Delete structs for setting pins, changes funcs CAN and added functions for mapping pins

--- ttcsoft/src/CAN_Properties.c
+++ ttcsoft/src/CAN_Properties.c
@@ -1,6 +1,6 @@
 #include "CAN_Properties.h"
 
-// Генерация ID с битовой маской
+// Generate ID with bits mask
 
 void Generate_Group_IDs(ubyte1 index, ubyte4 *ptr_CMD, ubyte4 *ptr_FB,
                                ubyte4 *ptr_DIAG)
@@ -8,24 +8,23 @@ void Generate_Group_IDs(ubyte1 index, ubyte4 *ptr_CMD, ubyte4 *ptr_FB,
     ubyte4 base_cmd = 0x18FF00FAU;
     ubyte4 base_fb = 0x18FF0027U;
     ubyte4 base_diag = 0x18FF0027U;
-    ubyte4 mask = 0x0000FF00U; // Биты 8-15
+    ubyte4 mask = 0x0000FF00U; // Bits 8-15
 
     *ptr_CMD = (base_cmd & ~mask) | ((ubyte4) index << 8);
     *ptr_FB = (base_fb & ~mask) | ((ubyte4) (index * 2) << 8);
     *ptr_DIAG = (base_diag & ~mask) | ((ubyte4) ((index * 2) + 1) << 8);
 }
 
-// Настрорйка кадра по дефолту и подстановка айдишника
+// Settings messages by default
 
 void Init_CAN_Frame_Defaults(IO_CAN_DATA_FRAME *frame, ubyte4 id)
 {
     frame->id = id;
     frame->id_format = IO_CAN_EXT_FRAME;
     frame->length = 8U;
-    // data[8] обнулится или перезапишется перед отправкой
 }
 
-// Функции упаковки и распаковки
+// Functions for pack and unpack massages
 
 void Unpack_CMD(const ubyte1 *can_data, CMD_Values *cmd)
 {
@@ -63,108 +62,141 @@ void Pack_DIAG(const DIAG_Values *diag, ubyte1 *can_data)
     can_data[7] = (ubyte1) ((diag->val_Pin3 >> 8) & 0xFFU);
 }
 
-// Главная функция инициализации
+// MAY BE DELETED??7 YOU SHOULD TO SEE ERTMAIN.C AND THINKING!!!!!!!!!!
 
-IO_ErrorType Init_CAN_System(void)
+/*
+ * Initialization of physical CAN channels.
+ * Called ONCE at system startup.
+ */
+IO_ErrorType Init_CAN_Channels(void)
 {
+    IO_ErrorType err;
+
+    // Channel 0: RX (CMD) + TX (FB)
+    IO_CAN_Init(IO_CAN_CHANNEL_0, IO_CAN_BIT_250_KB, 0, 0, 0, 0);
+    if (err != IO_E_OK)
+        return err;
+
+    // Channel 1: TX (DIAG)
+    IO_CAN_Init(IO_CAN_CHANNEL_1, IO_CAN_BIT_250_KB, 0, 0, 0, 0);
+    return err;
+}
+
+/*
+ * Initialization of ONE specific group.
+ * Called for each group separately.
+ * @param group_idx Group index (0 .. NUM_GROUPS-1)
+ */
+
+IO_ErrorType Init_CAN_Group(ubyte1 group_idx)
+{
+    IO_ErrorType err;
+
+    // Protection against array out of bounds
+    if (group_idx >= NUM_GROUPS)
+    {
+        return IO_E_INVALID_PARAMETER;
+    }
+
+    // 1. Generate IDs for this group
+    Generate_Group_IDs(group_idx, &g_groups[group_idx].can.frame_CMD.id,
+                       &g_groups[group_idx].can.frame_FB.id,
+                       &g_groups[group_idx].can.frame_DIAG.id);
+
+    // 2. Fill frame templates
+    Init_CAN_Frame_Defaults(&g_groups[group_idx].can.frame_CMD,
+                            g_groups[group_idx].can.frame_CMD.id);
+    Init_CAN_Frame_Defaults(&g_groups[group_idx].can.frame_FB,
+                            g_groups[group_idx].can.frame_FB.id);
+    Init_CAN_Frame_Defaults(&g_groups[group_idx].can.frame_DIAG,
+                            g_groups[group_idx].can.frame_DIAG.id);
+
+    // 3. Configure reception buffer (CMD) on Channel 0
+    err = IO_CAN_ConfigMsg(&g_groups[group_idx].can.handle_CMD,
+                           IO_CAN_CHANNEL_0,
+                           IO_CAN_MSG_READ,
+                           IO_CAN_EXT_FRAME,
+                           g_groups[group_idx].can.frame_CMD.id, 0x1FFFFFFFU); // Strict mask
+    if (err != IO_E_OK)
+        return err;
+
+    // 4. Configure transmission buffer (FB) on Channel 0
+    err = IO_CAN_ConfigMsg(&g_groups[group_idx].can.handle_FB, IO_CAN_CHANNEL_0,
+    IO_CAN_MSG_WRITE,
+                           IO_CAN_EXT_FRAME,
+                           g_groups[group_idx].can.frame_FB.id, 0U);
+    if (err != IO_E_OK)
+        return err;
+
+    // 5. Configure transmission buffer (DIAG) on Channel 1
+    err = IO_CAN_ConfigMsg(&g_groups[group_idx].can.handle_DIAG,
+                           IO_CAN_CHANNEL_1,
+                           IO_CAN_MSG_WRITE,
+                           IO_CAN_EXT_FRAME,
+                           g_groups[group_idx].can.frame_DIAG.id, 0U);
+
+    return err;
+}
+
+/*
+ * CONVENIENT WRAPPER: Initialization of the ENTIRE system at once.
+ * If you don't need step-by-step initialization, just call this function.
+ */
+IO_ErrorType Init_CAN_System_All(void)
+{
+    IO_ErrorType err = Init_CAN_Channels();
+    if (err != IO_E_OK)
+        return err;
 
     ubyte1 i;
 
-    // 1. Инициализация Канала 0 (RX CMD + TX FB) на 250 кбит/с
-    IO_CAN_Init(IO_CAN_CHANNEL_0, IO_CAN_BIT_250_KB, 0, 0, 0, 0);
-
-    // 2. Инициализация Канала 1 (TX DIAG) на 250 кбит/с
-    IO_CAN_Init(IO_CAN_CHANNEL_1, IO_CAN_BIT_250_KB, 0, 0, 0, 0);
-
-    // 3. Настройка 24 групп
     for (i = 0; i < NUM_GROUPS; i++)
     {
-
-        // Генерация уникальных ID
-        Generate_Group_IDs(i, &g_groups[i].can.frame_CMD.id,
-                           &g_groups[i].can.frame_FB.id,
-                           &g_groups[i].can.frame_DIAG.id);
-
-        // Заполнение полей кадра по дефолту (length=8, id_format=EXT)
-
-        Init_CAN_Frame_Defaults(&g_groups[i].can.frame_CMD,
-                                g_groups[i].can.frame_CMD.id);
-        Init_CAN_Frame_Defaults(&g_groups[i].can.frame_FB,
-                                g_groups[i].can.frame_FB.id);
-        Init_CAN_Frame_Defaults(&g_groups[i].can.frame_DIAG,
-                                g_groups[i].can.frame_DIAG.id);
-
-        // Канал 0: Прием (CMD)
-
-        IO_CAN_ConfigMsg(&g_groups[i].can.handle_CMD, IO_CAN_CHANNEL_0,
-        IO_CAN_MSG_READ,
-                         IO_CAN_EXT_FRAME, g_groups[i].can.frame_CMD.id,
-                         0x1FFFFFFFU);
-
-        // Канал 0: Передача (FB)
-
-        IO_CAN_ConfigMsg(&g_groups[i].can.handle_FB, IO_CAN_CHANNEL_0,
-        IO_CAN_MSG_WRITE,
-                         IO_CAN_EXT_FRAME, g_groups[i].can.frame_FB.id, 0U);
-
-        // Канал 1: Передача (DIAG)
-
-        IO_CAN_ConfigMsg(&g_groups[i].can.handle_DIAG, IO_CAN_CHANNEL_1,
-        IO_CAN_MSG_WRITE,
-                         IO_CAN_EXT_FRAME, g_groups[i].can.frame_DIAG.id, 0U);
-
+        err = Init_CAN_Group(i);
+        if (err != IO_E_OK)
+            return err; // Break at the first error
     }
-
     return IO_E_OK;
 }
 
-// Основной цикл приема и передачи
+// Read massage method
 
-void Cyclic_CAN_Task(void)
+void Process_CAN_RX(ubyte1 group_idx)
 {
-    ubyte1 i;
+    if (group_idx >= NUM_GROUPS)
+        return;
 
-    // Локальная переменная только для приема
-    IO_CAN_DATA_FRAME rx_frame;
-
-    for (i = 0; i < NUM_GROUPS; i++)
+    if (IO_CAN_MsgStatus(g_groups[group_idx].can.handle_CMD) == IO_E_OK)
     {
-
-        // Чтение (RX)
-
-        if (IO_CAN_MsgStatus(g_groups[i].can.handle_CMD) == IO_E_OK)
+        // Read DIRECTLY into the global template (without local variables)
+        if (IO_CAN_ReadMsg(g_groups[group_idx].can.handle_CMD,
+                           &g_groups[group_idx].can.frame_CMD) == IO_E_OK)
         {
-            if (IO_CAN_ReadMsg(g_groups[i].can.handle_CMD, &rx_frame) == IO_E_OK)
-            {
 
-                // Сверка ID и длины
-                if ((rx_frame.length == 8U)
-                        && (rx_frame.id == g_groups[i].can.frame_CMD.id))
-                {
-                    Unpack_CMD(rx_frame.data, &g_groups[i].cmd);
-                    // Данные готовы: g_groups[i].cmd.config_Pin0, value_Pin0 и т.д.
-                }
+            if (g_groups[group_idx].can.frame_CMD.length == 8U)
+            {
+                Unpack_CMD(g_groups[group_idx].can.frame_CMD.data,
+                           &g_groups[group_idx].cmd);
             }
         }
-
-        // 2. Запись данных после снятия с пинов и конвертации (FB) на Канале 0
-
-        // 1. Упаковываем
-
-        Pack_FB(&g_groups[i].fb, g_groups[i].can.frame_FB.data);
-
-        // 2. Отправляем (ID, length и id_format уже настроены при инициализации)
-        IO_CAN_WriteMsg(g_groups[i].can.handle_FB, &g_groups[i].can.frame_FB);
-
-
-        // 3. Запись диагностики (DIAG) на Канале 1
-
-        // 1. Упаковываем
-        Pack_DIAG(&g_groups[i].diag, g_groups[i].can.frame_DIAG.data);
-
-        // 2. Отправляем
-        IO_CAN_WriteMsg(g_groups[i].can.handle_DIAG,
-
-        &g_groups[i].can.frame_DIAG);
     }
+}
+
+// Write massage method
+
+void Process_CAN_TX(ubyte1 group_idx)
+{
+    if (group_idx >= NUM_GROUPS)
+        return;
+
+    // FB (Channel 0)
+    Pack_FB(&g_groups[group_idx].fb, g_groups[group_idx].can.frame_FB.data);
+    IO_CAN_WriteMsg(g_groups[group_idx].can.handle_FB,
+                    &g_groups[group_idx].can.frame_FB);
+
+    // DIAG (Channel 1)
+    Pack_DIAG(&g_groups[group_idx].diag,
+              g_groups[group_idx].can.frame_DIAG.data);
+    IO_CAN_WriteMsg(g_groups[group_idx].can.handle_DIAG,
+                    &g_groups[group_idx].can.frame_DIAG);
 }
